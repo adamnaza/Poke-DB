108e3623c3273d4071a39be6a1ecb7eadefadfeb
adamchenaza <109355623+adamnaza@users.noreply.github.com>
2025-06-30 15:28:02 -0400

    final

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: ['raw.githubusercontent.com'],
+    },
+    reactStrictMode: true,
+
+};
 
 export default nextConfig;
